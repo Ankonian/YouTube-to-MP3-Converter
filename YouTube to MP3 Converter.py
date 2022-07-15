d9779cc724caef2b06ad7ce547a13ebfc134697a
lli76 <langli1307@gmail.com>
2022-07-14 18:52:15 -0700

    User Input spacing changes

--- YouTube to MP3 Converter.py
+++ YouTube to MP3 Converter.py
@@ -4,14 +4,14 @@ import tkinter as tk
 
 
 #Ask user for the YouTube link
-link = str(input("Enter the URL of the video you wish to convert: \n"))
+link = str(input("Enter the URL of the video you wish to convert:\n"))
 yt=YouTube(link)
 
 #Grab the second audio only stream(highest bitrate mp4 format)
 audio = yt.streams.filter(only_audio=True).__getitem__(1)
 
 #Ask user where to save the mp3 file
-destination = str(input("Enter the output path, leave blank for current directory:\n ")) or '.'
+destination = str(input("Enter the output path, leave blank for current directory:\n")) or '.'
 downloaded_file = audio.download(output_path=destination)
 
 #File grabbed from pytube stream will be in mp4 format, hard convert to mp3
